47cd3b66a0be0fb25e78cd0ffbfd28fd18053e7c
Jacob <jperez32017@gmail.com>
2023-09-05 21:41:52 -0700

    Added new folders, apps, urls, views

---
+++ item/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Item
+
+# Create your views here.
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    return render(request, 'Item/detail.html', {
+        'item': item
+    })
